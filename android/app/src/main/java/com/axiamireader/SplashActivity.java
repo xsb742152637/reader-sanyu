86b19d7c4b59a2778f76c2b3a7da69fc33d2d716
谢世兵 <742152637@qq.com>
2019-01-10 17:15:03 +0800

    开屏广告调整

--- android/app/src/main/java/com/axiamireader/SplashActivity.java
+++ android/app/src/main/java/com/axiamireader/SplashActivity.java
@@ -45,20 +45,22 @@ public class SplashActivity extends Activity implements SplashADListener {
     private ImageView splashHolder;
     private static final String SKIP_TEXT = "跳过%d";
 
-    public boolean canJump = true;
+    public boolean canJump = true;//是否关闭开屏广告
 
     /**
      * 为防止无广告时造成视觉上类似于"闪退"的情况，设定无广告时页面跳转根据需要延迟一定时间，demo
      * 给出的延时逻辑是从拉取广告开始算开屏最少持续多久，仅供参考，开发者可自定义延时逻辑，如果开发者采用demo
      * 中给出的延时逻辑，也建议开发者考虑自定义minSplashTimeWhenNoAD的值（单位ms）
      **/
-    private int minSplashTimeWhenNoAD = 2000;
+    private int minSplashTimeWhenNoAD = 1000;
     /**
      * 记录拉取广告的时间
      */
     private long fetchSplashADTime = 0;
     private Handler handler = new Handler(Looper.getMainLooper());
 
+    //android部分生命周期
+    //onCreate > onResume > onPause > onDestroy
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -67,12 +69,33 @@ public class SplashActivity extends Activity implements SplashADListener {
         skipView = (TextView) findViewById(R.id.skip_view);
         splashHolder = (ImageView) findViewById(R.id.splash_holder);
 
-        sendRequestWithHttpClient();
+        showSplahAd();
+//        sendRequestWithHttpClient();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (canJump) {
+            next();
+        }
+        canJump = true;
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        canJump = false;
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        handler.removeCallbacksAndMessages(null);
     }
 
     private void sendRequestWithHttpClient() {
         new Thread(new Runnable() {
-
             @Override
             public void run() {
                 try {
@@ -85,16 +108,13 @@ public class SplashActivity extends Activity implements SplashADListener {
                     }else{
                         showSplahAd();
                     }
-
                 } catch (Exception e) {
                     // TODO Auto-generated catch block
 //                    e.printStackTrace();
                     startReader();
                 }
-
             }
         }).start();//这个start()方法不要忘记了
-
     }
 
     private void showSplahAd() {
@@ -188,12 +208,14 @@ public class SplashActivity extends Activity implements SplashADListener {
         splashAD = new SplashAD(activity, adContainer, skipContainer, appId, posId, adListener, fetchDelay);
     }
 
+    //广告成功展示时调用，成功展示不等于满足计费条件（如展示时长尚未满足），隐藏预设开屏图片
     @Override
     public void onADPresent() {
         Log.i("AD_DEMO", "SplashADPresent");
         splashHolder.setVisibility(View.INVISIBLE); // 广告展示后一定要把预设的开屏图片隐藏起来
     }
 
+    //广告被点击时调用，不代表满足计费条件（如点击时网络异常）
     @Override
     public void onADClicked() {
         Log.i("AD_DEMO", "SplashADClicked");
@@ -216,18 +238,17 @@ public class SplashActivity extends Activity implements SplashADListener {
         Log.i("AD_DEMO", "SplashADExposure");
     }
 
+    //广告关闭时调用，可能是用户关闭或者展示时间到。此时一般需要跳过开屏的Activity，进入应用内容页面
     @Override
     public void onADDismissed() {
         Log.i("AD_DEMO", "SplashADDismissed");
         next();
     }
 
+    //广告加载失败，error对象包含了错误码和错误信息
     @Override
     public void onNoAD(AdError error) {
-        Log.i(
-                "AD_DEMO",
-                String.format("LoadSplashADFail, eCode=%d, errorMsg=%s", error.getErrorCode(),
-                        error.getErrorMsg()));
+        Log.i("AD_DEMO", String.format("LoadSplashADFail, eCode=%d, errorMsg=%s", error.getErrorCode(), error.getErrorMsg()));
         /**
          * 为防止无广告时造成视觉上类似于"闪退"的情况，设定无广告时页面跳转根据需要延迟一定时间，demo
          * 给出的延时逻辑是从拉取广告开始算开屏最少持续多久，仅供参考，开发者可自定义延时逻辑，如果开发者采用demo
@@ -288,27 +309,6 @@ public class SplashActivity extends Activity implements SplashADListener {
         }
     }
 
-    @Override
-    protected void onPause() {
-        super.onPause();
-        canJump = false;
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        if (canJump) {
-            next();
-        }
-        canJump = true;
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        handler.removeCallbacksAndMessages(null);
-    }
-
     /**
      * 开屏页一定要禁止用户对返回按钮的控制，否则将可能导致用户手动退出了App而广告无法正常曝光和计费
      */
@@ -321,229 +321,3 @@ public class SplashActivity extends Activity implements SplashADListener {
     }
 
 }
-
-
-//import android.view.KeyEvent;
-//import android.view.View;
-//import android.widget.FrameLayout;
-//import android.widget.ImageView;
-//import android.widget.TextView;
-//import android.widget.Toast;
-//
-//import com.anzhi.sdk.ad.main.AzSplashAd;
-//import com.anzhi.sdk.ad.manage.AnzhiAdCallBack;
-//import com.axiamireader.AdIDs;
-//import com.axiamireader.MainActivity;
-//
-//import java.util.ArrayList;
-//import java.util.List;
-//
-//
-//public class SplashActivity extends Activity {
-//    private AzSplashAd splshAd;
-//    TextView tx;
-//    ImageView holder;
-//    private boolean next;
-//
-//    private boolean isFront = false;//这个值表示页面是否在前台（true 表示在，false 表示不在前台）
-//
-//    private boolean isClosed = false;//该值表示广告是否关闭（isClosed 为 true 表示该广告关闭了，false 表示该广告没有关闭）
-//
-//
-//    @Override
-//    protected void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        setContentView(R.layout.splash_activity_ly);
-//        holder = (ImageView) findViewById(R.id.anzhi_splash_holder);
-//        tx = (TextView) findViewById(R.id.anzhi_skip_view);
-//
-//        // 如果targetSDKVersion >= 23，就要申请好权限。如果您的App没有适配到Android6.0（即targetSDKVersion < 23）。
-//        if (Build.VERSION.SDK_INT >= 23) {
-//            checkAndRequestPermission();
-//        } else {
-//            // 如果是Android6.0以下的机器，默认在安装时获得了所有权限，可以直接调用SDK
-//            loadAdSplash();
-//        }
-//    }
-//
-//    private void loadAdSplash() {
-//        splshAd = new AzSplashAd(this, AdIDs.APPKEY, AdIDs.SPLASH_ID, new AnzhiAdCallBack() {
-//            @Override
-//            public void onShow() {
-//                holder.setVisibility(View.INVISIBLE);
-//                Log.e("","--闪屏广告展示---");
-//            }
-//
-//            @Override
-//            public void onReceiveAd() {
-//            }
-//
-//            @Override
-//            public void onLoadFailed(String result) {
-//                Log.e("","--闪屏广告加载失败---原因----：" + result);
-//                toNextActivity();
-//            }
-//
-//            /*
-//             *
-//             * 关闭广告
-//             */
-//            @Override
-//            public void onCloseAd() {
-//                toNextActivity();
-//                Log.e("","--闪屏广告关闭");
-//            }
-//
-//            @Override
-//            public void onAdExposure() {
-//
-//            }
-//
-//            @Override
-//            public void onADTick(long millisUntilFinished) {
-//                tx.setText(String.format("点击跳过 %d", Math.round(millisUntilFinished / 1000f)));
-//            }
-//
-//            @Override
-//            public void onAdClik() {
-//                Log.e("","---闪屏广告被点击---");
-//            }
-//        }, (FrameLayout) findViewById(R.id.anzhi_splash_container), tx);
-//        splshAd.setDelayTimes(5);
-//        splshAd.loadAd();
-//    }
-//
-//    /**
-//     * 跳转到新的页面
-//     */
-//    private void toNextActivity() {
-//        isClosed = true;
-//        if (isFront) {
-//            if (next) {
-//                return;
-//            }
-//            next = true;
-//            finish();
-//
-//            try{
-//                Bundle myBundelForGetName=this.getIntent().getExtras();
-//                String name = myBundelForGetName.getString("display_type");
-//
-//                Log.e("afanna","display type = " + name);
-//
-//                if (name.equals("react")) {
-//                    Log.e("afanna","display type is react");
-//                    return;
-//                }else{
-//                    Log.e("afanna","display type is not react, start reader");
-//                    startReader();
-//                }
-//            }catch (NullPointerException ex) {
-//                Log.e("afanna",ex.getMessage());
-//                startReader();
-//            }
-//        }
-//    }
-//
-//    public void startReader() {
-//        Intent intent = new Intent();
-//        intent.setClass(SplashActivity.this, MainActivity.class);
-//        SplashActivity.this.startActivity(intent);
-//        SplashActivity.this.overridePendingTransition(0, 0);
-//    }
-//
-//    @Override
-//    protected void onResume() {
-//        super.onResume();
-//        isFront = true;
-//        if (isClosed) {
-//            toNextActivity();
-//        }
-//        if (splshAd != null) {
-//            splshAd.onResume();
-//        }
-//    }
-//
-//    @Override
-//    protected void onDestroy() {
-//        splshAd.onDestroy();
-//        super.onDestroy();
-//    }
-//
-//    @Override
-//    protected void onPause() {
-//        isFront = false;
-//        super.onPause();
-//    }
-//
-//    /**
-//     * ----------非常重要----------
-//     * <p>
-//     * Android6.0以上的权限适配简单示例：
-//     * <p>
-//     * 如果targetSDKVersion >= 23，那么必须要申请到所需要的权限，再调用广点通SDK，否则广点通SDK不会工作。
-//     * <p>
-//     * Demo代码里是一个基本的权限申请示例，请开发者根据自己的场景合理地编写这部分代码来实现权限申请。
-//     * 注意：下面的`checkSelfPermission`和`requestPermissions`方法都是在Android6.0的SDK中增加的API，如果您的App还没有适配到Android6.0以上，则不需要调用这些方法，直接调用广点通SDK即可。
-//     */
-//    @TargetApi(Build.VERSION_CODES.M)
-//    private void checkAndRequestPermission() {
-//        List<String> lackedPermission = new ArrayList<String>();
-//        if (!(checkSelfPermission(Manifest.permission.READ_PHONE_STATE) == PackageManager.PERMISSION_GRANTED)) {
-//            lackedPermission.add(Manifest.permission.READ_PHONE_STATE);
-//        }
-//
-//        if (!(checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED)) {
-//            lackedPermission.add(Manifest.permission.WRITE_EXTERNAL_STORAGE);
-//        }
-//
-//        if (!(checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED)) {
-//            lackedPermission.add(Manifest.permission.ACCESS_FINE_LOCATION);
-//        }
-//
-//        // 权限都已经有了，那么直接调用SDK
-//        if (lackedPermission.size() == 0) {
-//            loadAdSplash();
-//        } else {
-//            // 请求所缺少的权限，在onRequestPermissionsResult中再看是否获得权限，如果获得权限就可以调用SDK，否则不要调用SDK。
-//            String[] requestPermissions = new String[lackedPermission.size()];
-//            lackedPermission.toArray(requestPermissions);
-//            requestPermissions(requestPermissions, 1024);
-//        }
-//    }
-//
-//    private boolean hasAllPermissionsGranted(int[] grantResults) {
-//        for (int grantResult : grantResults) {
-//            if (grantResult == PackageManager.PERMISSION_DENIED) {
-//                return false;
-//            }
-//        }
-//        return true;
-//    }
-//
-//    @Override
-//    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
-//        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-//        if (requestCode == 1024 && hasAllPermissionsGranted(grantResults)) {
-//            loadAdSplash();
-//        } else {
-//            // 如果用户没有授权，那么应该说明意图，引导用户去设置里面授权。
-//            Toast.makeText(this, "应用缺少必要的权限！请点击\"权限\"，打开所需要的权限。", Toast.LENGTH_LONG).show();
-//            Intent intent = new Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
-//            intent.setData(Uri.parse("package:" + getPackageName()));
-//            startActivity(intent);
-//            finish();
-//        }
-//    }
-//
-//    /**
-//     * 防止用户返回键退出APP
-//     */
-//    @Override
-//    public boolean onKeyDown(int keyCode, KeyEvent event) {
-//        if (keyCode == KeyEvent.KEYCODE_BACK || keyCode == KeyEvent.KEYCODE_HOME) {
-//            return true;
-//        }
-//        return super.onKeyDown(keyCode, event);
-//    }
-//}
