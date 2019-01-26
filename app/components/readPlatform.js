f60b0f4fda2b1192285bc3d45e2a2c3d48ef06eb
lzz <742152637@qq.com>
2019-01-26 10:44:19 +0800

    调整

--- app/components/readPlatform.js
+++ app/components/readPlatform.js
@@ -114,6 +114,11 @@ export default class ReadPlatform extends Component {
         }
 
         let bookId = this.props.bookId;
+        bookId = null;
+        if(bookId == undefined || bookId == "undefined" || bookId == null || bookId == "null" || bookId == ""){
+            Toast.toastShort('小说编号：'+bookId+' 无法识别~~')
+            return;
+        }
         let book = realm.objectForPrimaryKey('HistoryBook', bookId);
         let source;
         let isMainApi = true;
