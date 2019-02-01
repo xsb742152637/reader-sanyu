3dcdd4a4ce9643245cee686ed39a0902342ecb1c
xc <742152637@qq.com>
2019-02-01 23:56:26 +0800

    重复启动

--- android/app/src/main/java/com/axiamireader/MainActivity.java
+++ android/app/src/main/java/com/axiamireader/MainActivity.java
@@ -1,17 +1,13 @@
 package com.axiamireader;
 
-import android.app.ActivityManager;
-import android.content.Context;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.KeyEvent;
-import android.widget.Toast;
 
 import com.facebook.react.ReactActivity;
 import com.umeng.analytics.MobclickAgent;
 
 public class MainActivity extends ReactActivity {
-    private long exitTime = 0;
+
     /**
      * Returns the name of the main component registered from JavaScript.
      * This is used to schedule rendering of the component.
@@ -42,31 +38,5 @@ public class MainActivity extends ReactActivity {
     protected void onDestroy() {
         super.onDestroy();
         Log.i("AD_DEMO", "MainActivity-------------onDestroy");
-    }
-
-    @Override
-    public boolean onKeyDown(int keyCode, KeyEvent event) {
-        if (keyCode == KeyEvent.KEYCODE_BACK) {
-            exit();
-            return false;
-        }
-        return super.onKeyDown(keyCode, event);
-    }
-
-    public void exit() {
-        if ((System.currentTimeMillis() - exitTime) > 2000) {
-            Toast.makeText(getApplicationContext(), "再按一次退出程序11",
-                    Toast.LENGTH_SHORT).show();
-            exitTime = System.currentTimeMillis();
-        } else {
-            Toast.makeText(getApplicationContext(), "退出程序22",
-                    Toast.LENGTH_SHORT).show();
-//            finish();
-            ActivityManager manager = (ActivityManager) getSystemService(Context.ACTIVITY_SERVICE);
-            manager.restartPackage(getPackageName());
-
-            android.os.Process.killProcess(android.os.Process.myPid());
-//            System.exit(0);
-        }
     }
 }
